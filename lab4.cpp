67679967756563ab02c04852c0e8af657e4a3422
enindaniil07-sudo <enin.daniil07@gmail.com>
2025-10-18 00:58:47 +0200

    Add files via upload

---
+++ lab4.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+int main() {
+    int N;
+
+    std::cin >> N;
+
+    if (std::cin.fail() || N < 1 || N > 30) {
+        std::cout << "ERROR" << std::endl;
+        return 1;
+    }
+    int f1 = 0;//первое число
+    int f2 = 1;//второе число
+    int k = 0;//номер числа Фибоначчи
+
+    while (k <= N) {
+        if (f2 % 2 != 0) {
+            std::cout << f2 << std::endl;
+            k++;
+        }
+
+        int c = f1 + f2;
+        f1 = f2;
+        f2 = c;
+    }
+    return 0;
+}
